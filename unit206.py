01872596113afe5d9c16ba1c5a4a5ea309401388
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-02-27 14:02:39 -0500

    Initial commit

---
+++ unit206.py
@@ -0,0 +1,11 @@
+#Created By: Amin Zeina 
+#Created On: Feb 2018
+#Calculate area of a trapezoid
+
+aBase = int(input( "Enter A: "))
+bBase = int(input( "Enter B: "))
+Height = int(input( "Enter Height: "))
+
+print( (aBase + bBase) / 2 * Height)
+
+input( "Program End")
